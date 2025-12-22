ad35003b883019543139d6edc1e811a7eeb9c725
jensdeg <jensdegraaf88@gmail.com>
2025-12-22 15:17:38 +0100

    Day2Part1

---
+++ src/Day2/Program.cs
@@ -0,0 +1,23 @@
+﻿var content = File.ReadAllText("Input.txt");
+var ranges = content.Split(',');
+
+long InvalidIds = 0;
+
+foreach (var range in ranges)
+{
+    var start = long.Parse(range.Split('-')[0]);
+    var end = long.Parse(range.Split("-")[1]);
+    for(long id = start; id < end; id++)
+    {
+        var idString = id.ToString();
+        if (idString.Length % 2 != 0) continue;
+
+        var left = idString[..(idString.Length / 2)];
+        var right = idString[(idString.Length / 2)..];
+
+        if (left == right) InvalidIds += id;
+
+    }
+}
+
+Console.WriteLine(InvalidIds);

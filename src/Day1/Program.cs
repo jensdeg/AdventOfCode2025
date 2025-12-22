fdc4c0d5ff1e3ab9b7d0c80acec76e3e98c4f431
jensdeg <jensdegraaf88@gmail.com>
2025-12-22 14:55:57 +0100

    Day 1

---
+++ src/Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿var content = File.ReadAllText("Input.txt");
+var lines = content.Split(Environment.NewLine);
+
+var DialNumber = 50;
+var ZeroCount = 0;
+
+foreach(var line in lines)
+{
+    if (line.StartsWith('L'))
+    {
+        int value = int.Parse(line.Split('L')[1]);
+        Subtract(value);
+    }
+    else
+    {
+        int value = int.Parse(line.Split('R')[1]);
+        Add(value);
+    }
+}
+
+Console.WriteLine(ZeroCount);
+
+
+void Subtract(int count)
+{
+    for(int i = 0; i < count; i++)
+    {
+        DialNumber--;
+        if(DialNumber < 0)
+        {
+            DialNumber = 99;
+        }
+    }
+    if(DialNumber == 0) ZeroCount++;
+}
+
+void Add(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        DialNumber++;
+        if (DialNumber > 99)
+        {
+            DialNumber = 0;
+        }
+    }
+    if (DialNumber == 0) ZeroCount++;
+}
